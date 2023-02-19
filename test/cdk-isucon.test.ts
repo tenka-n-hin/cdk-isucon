9323712fbc17bfd50d9c5a4f5e4c3ad487b9aece
Masayoshi Wada <developer@andantesoftware.com>
2023-02-19 11:53:31 +0900

    Initial commit

---
+++ test/cdk-isucon.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkIsucon from '../lib/cdk-isucon-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-isucon-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkIsucon.CdkIsuconStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
